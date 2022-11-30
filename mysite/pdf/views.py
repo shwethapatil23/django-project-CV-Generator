5d8c8e352ef97563d250921422cdde6002c0cf17
unknown <shwetha.np23@gmail.com>
2022-11-30 18:57:51 +0530

    first

---
+++ mysite/pdf/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+import pdfkit
+from .models import Profile
+from django.http import HttpResponse
+from django.template import loader
+
+# config = pdfkit.configuration(wkhtmltopdf=r"C:\Users\sgouldson.CASPIAN71\Documents\coding\udemy\django_course\CV_generator\wkhtmltopdf\bin")
+import io
+import os
+
+# Create your views here.
+
+def accept(request):
+    if request.method == "POST":
+        name = request.POST.get("name", "")
+        email = request.POST.get("email", "")
+        phone = request.POST.get("phone", "")
+        summary = request.POST.get("summary", "")
+        degree = request.POST.get("degree", "")
+        school = request.POST.get("school", "")
+        university = request.POST.get("university", "")
+        previous_work = request.POST.get("previous_work", "")
+        skills = request.POST.get("skills", "")
+
+        profile = Profile(name=name, email=email, phone=phone, summary=summary, degree=degree, school=school, university=university, previous_work=previous_work, skills=skills)
+        profile.save()
+
+    return render(request, 'pdf/accept.html')
+
+def resume(request,id):
+    user_profile = Profile.objects.get(pk=id)
+    template = loader.get_template('pdf/resume.html')
+    html = template.render({'user_profile':user_profile})
+    options ={
+        'page-size':'Letter',
+        'encoding':"UTF-8",
+    }
+
+
+    pdf1 = pdfkit.from_string(html,False,options)
+    response = HttpResponse(pdf1,content_type='application/pdf')
+    response['Content-Disposition'] ='attachment;filename=resume.pdf'
+
+    return response
+
+def list(request):
+    profiles = Profile.objects.all()
+    return render(request, 'pdf/list.html', {'profiles': profiles})
